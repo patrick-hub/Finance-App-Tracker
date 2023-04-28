17b31db36b26239fec1f9e16883bcd6fea7e5b82
MAC <mac@MACs-MacBook-Pro.local>
2023-04-28 11:38:46 +0100

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCAst0zY5p_OiUeQ5NY1O5QdvO4sPrsdMU",
+    authDomain: "finance-app-tracker-bdd4a.firebaseapp.com",
+    projectId: "finance-app-tracker-bdd4a",
+    storageBucket: "finance-app-tracker-bdd4a.appspot.com",
+    messagingSenderId: "445070371653",
+    appId: "1:445070371653:web:e2e6ef66e7bf3708f82c2c"
+  };
+
+  // initialise firebase using firebase config
+  firebase.initializeApp(firebaseConfig)
+
+  // initialise individual service
+  const projectFirestore = firebase.firestore()
+
+export {projectFirestore}
